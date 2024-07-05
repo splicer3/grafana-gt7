b152882a042f5d5cbd9fd40d011f63e34f5375dd
Matteo Di Iorio <matteo.diiorio2@studio.unibo.it>
2024-07-05 19:05:01 +0200

    feat: First GT7 telemetry server prototype todo: Playstation IP is hardcoded for now, will update after testing

---
+++ pkg/gt7/telemetry-server.go
@@ -0,0 +1,49 @@
+package gt7
+
+import (
+	"fmt"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+	"net"
+)
+
+const (
+	serverPort    = "33740"
+	playstationIP = "192.168.1.5"
+)
+
+func RunTelemetryServer(ch chan TelemetryFrame, errCh chan error) {
+	port := fmt.Sprintf("%s:%s", playstationIP, serverPort)
+	s, err := net.ResolveUDPAddr("udp4", port)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	connection, err := net.ListenUDP("udp4", s)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	log.DefaultLogger.Info("Starting telemetry server for Gran Turismo 7")
+
+	defer connection.Close()
+	buffer := make([]byte, 4096)
+
+	for {
+		n, _, err := connection.ReadFromUDP(buffer)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		fmt.Printf("Read %v bytes\n", n)
+
+		packetBuffer := buffer[0:n]
+		p, err := ReadPacket(packetBuffer)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		ch <- *p
+	}
+}
